86cfd8cb5b992f05398fd42d8ff1bd70002a563d
Eric Fritz <eric@eric-fritz.com>
2019-07-28 19:52:10 -0700

    Seaprate analysis from eLSIF emission. Improve docs and debug output.

---
+++ lsif_exporter/analysis.py
@@ -0,0 +1,57 @@
+import jedi
+
+
+class Name:
+    """
+    An object that represents a reference or definition of a
+    variable in a particular source file.
+    """
+    def __init__(self, definition):
+        self.definition = definition
+
+    def is_definition(self):
+        """
+        Return true if this name is a definition or assignment.
+        """
+        return self.definition.is_definition()
+
+    def definitions(self):
+        """
+        Get a list of Name objects which define or assign this
+        particular usage of a variable reference. Generally, this
+        will only include one object. If this name is a definition,
+        the list will include itself.
+        """
+        return [Name(a) for a in self.definition.goto_assignments()]
+
+    @property
+    def line(self):
+        return self.definition.line - 1
+
+    @property
+    def lo(self):
+        return self.definition.column
+
+    @property
+    def hi(self):
+        return self.definition.column + len(self.definition.name)
+
+    def __eq__(self, other):
+        return self.definition == other.definition
+
+    def __hash__(self):
+        return self.definition.__hash__()
+
+
+def get_names(source, filename):
+    """
+    Retrieve a list of Name objects for the given source.
+    """
+    definitions = jedi.names(
+        source,
+        path=filename,
+        all_scopes=True,
+        references=True,
+    )
+
+    return [Name(d) for d in definitions]
--- lsif_exporter/file_export.py
+++ lsif_exporter/file_export.py
@@ -1,6 +1,8 @@
 import base64
-import jedi
 import os
+
+from .analysis import get_names
+from .consts import INDENT, MAX_HIGHLIGHT_RANGE
 
 
 class DefinitionMeta:
@@ -43,37 +45,56 @@ class FileExporter:
             base64.b64encode(source.encode('utf-8')).decode(),
         )
 
-        self.definitions = jedi.names(
-            source,
-            path=self.filename,
-            all_scopes=True,
-            references=True,
-        )
+        # Do an initial analysis to get a list of names from
+        # the source file. Some additional analysis may be
+        # done lazily in later steps when needed.
 
-        for definition in self.definitions:
-            if definition.is_definition():
-                self._export_definition(definition)
+        self.names = get_names(source, self.filename)
 
-            self._export_definition_uses(definition)
+        if self.verbose:
+            print('{}Searching for defs'.format(INDENT))
+
+        # First emit everything for names defined in this
+        # file. This needs to be done first as edges need
+        # to be emitted only after both of their adjacent
+        # vertices (i.e. defs must be emitted before uses).
+
+        for name in self.names:
+            if name.is_definition():
+                self._export_definition(name)
+
+        if self.verbose:
+            print('{}Searching for uses'.format(INDENT))
+
+        # Next, we can emit uses. Some of these names may
+        # reference a definition from another file or a
+        # builtin. The procedure below must account for
+        # these cases.
+
+        for name in self.names:
+            self._export_uses(name)
+
+        # Next, do any additional linking that we need to
+        # do now that both definition and their uses are
+        # emitted. Mainly, this populates hover tooltips
+        # for uses.
 
         for definition, meta in self.definition_metas.items():
-            self._link_definition_uses(definition, meta)
+            self._link_uses(definition, meta)
 
+        # Finally, link uses to their containing document
         self._emit_contains()
 
-    def _export_definition(self, definition):
+    def _export_definition(self, name):
         """
-        Emit vertices and edges related directly to a definition
-        or assignment of a variable. Create a definition meta object
+        Emit vertices and edges related directly to the definition of
+        or assignment to a variable. Create a definition meta object
         with the generated LSIF identifiers and make it queryable by
         the same definition object.
         """
-        # Print progress
-        self._debug_def(definition)
-
         contents = {
             'language': 'py',
-            'value': extract_definition_text(self.source_lines, definition),
+            'value': extract_text(self.source_lines, name),
         }
 
         # Emit hover tooltip and link it to a result set so that we can
@@ -83,55 +104,57 @@ class FileExporter:
         self.emitter.emit_textdocument_hover(result_set_id, hover_id)
 
         # Link result set to range
-        range_id = self.emitter.emit_range(*make_ranges(definition))
+        range_id = self.emitter.emit_range(*make_ranges(name))
         self.emitter.emit_next(range_id, result_set_id)
 
         # Stash the identifiers generated above so we can use then
         # when exporting related uses.
-        self.definition_metas[definition] = DefinitionMeta(
+        self.definition_metas[name] = DefinitionMeta(
             range_id,
             result_set_id,
             contents,
         )
 
-    def _export_definition_uses(self, definition):
+        # Print progress
+        self._debug_def(name)
+
+    def _export_uses(self, name):
         """
         Emit vertices and edges related to any use of a definition.
         The definition must have already been exported by the above
         procedure.
         """
         try:
-            assignments = definition.goto_assignments()
-        except Exception:
-            # TODO(efritz) - diagnose, document
-            print('OH WEIRD')
+            definitions = name.definitions()
+        except Exception as ex:
+            raise
+            print('Failed to retrieve definitions: {}'.format(str(ex)))
             return
 
-        for assignment in assignments:
-            self._export_use(definition, assignment)
+        for definition in definitions:
+            self._export_use(name, definition)
 
-    def _export_use(self, definition, assignment):
+    def _export_use(self, name, definition):
         """
         Emit vertices and edges directly related to a single use of
         a definition.
         """
-        meta = self.definition_metas.get(assignment)
+        meta = self.definition_metas.get(definition)
         if not meta:
-            # TODO(efritz) - what can we do here?
             return
 
         # Print progress
-        self._debug_use(definition, assignment)
+        self._debug_use(name, definition)
 
-        if definition.is_definition():
-            # The definition of the reference is the definition tiself.
-            # It is against spec to have overlapping or duplicate ranges
-            # in a single document, so we re-use the one that we had
-            # generated previously.
+        if name.is_definition():
+            # The use and the definition are the same. It is against
+            # spec to have overlapping or duplicate ranges in a single
+            # document, so we re-use the one that we had generated
+            # previously.
             range_id = meta.range_id
         else:
             # This must be a unique name, generate a new range vertex
-            range_id = self.emitter.emit_range(*make_ranges(definition))
+            range_id = self.emitter.emit_range(*make_ranges(name))
 
         # Link use range to definition resultset
         self.emitter.emit_next(range_id, meta.result_set_id)
@@ -146,7 +169,7 @@ class FileExporter:
         # Bookkeep this reference for the link procedure below
         meta.reference_range_ids.add(range_id)
 
-    def _link_definition_uses(self, definition, meta):
+    def _link_uses(self, name, meta):
         """
         Emit vertices and edges related to the relationship between a definition
         and it use(s).
@@ -193,71 +216,90 @@ class FileExporter:
     #
     # Debugging Methods
 
-    def _debug_def(self, definition):
+    def _debug_def(self, name):
         if not self.verbose:
             return
 
-        print('\tFound definition: (line {}) {}'.format(
-            definition.line,
-            highlight_range(self.source_lines, definition).strip()),
+        print('{}Def #{}, line {}: {}'.format(
+            INDENT * 2,
+            self.definition_metas.get(name).range_id,
+            name.line + 1,
+            highlight_range(self.source_lines, name).strip()),
         )
 
-    def _debug_use(self, definition, assignment):
-        if not self.verbose:
+    def _debug_use(self, name, definition):
+        if not self.verbose or name == definition:
             return
 
-        if assignment == definition:
-            assignment_summary = 'assigned by self'
-        else:
-            assignment_summary = 'assigned at (line {}) {}'.format(
-                assignment.line,
-                highlight_range(self.source_lines, assignment),
-            )
-
-        print('\tFound reference: (line {}) {} {}'.format(
-            definition.line,
-            highlight_range(self.source_lines, definition),
-            assignment_summary,
+        print('{}Use of #{}, line {}: {}'.format(
+            INDENT * 2,
+            self.definition_metas.get(definition).range_id,
+            name.line + 1,
+            highlight_range(self.source_lines, name),
         ))
 
 
-def make_ranges(definition):
+def make_ranges(name):
     """
     Return a start and end range values for a range vertex.
     """
-    lo = definition.column
-    hi = definition.column + len(definition.name)
-
     return (
-        {'line': definition.line - 1, 'character': lo},
-        {'line': definition.line - 1, 'character': hi},
+        {'line': name.line, 'character': name.lo},
+        {'line': name.line, 'character': name.hi},
     )
 
 
-def extract_definition_text(source_lines, definition):
+def extract_text(source_lines, name):
     """
-    Extract the text at the range described by the given definition.
+    Extract the text at the range described by the given name.
     """
-    lo = definition.column
-    hi = definition.column + len(definition.name)
-    line = source_lines[definition.line - 1]
-
-    # TODO(efritz) - capture comments
-    return line[lo:hi].rstrip()
+    line = source_lines[name.line]
+    # TODO(efritz) - trim line, capture comments
+    return line[name.lo:name.hi].rstrip()
 
 
-def highlight_range(source_lines, definition):
+def highlight_range(source_lines, name):
     """
-    Return the source line where the definition occurs with the
-    range described by the definition highlighted with an ANSI code.
+    Return the source line where the name occurs with the range
+    described by the name highlighted with an ANSI code.
     """
-    lo = definition.column
-    hi = definition.column + len(definition.name)
-    line = source_lines[definition.line - 1]
+    lo, hi = name.lo, name.hi
+    trimmed_lo, trimmed_hi = False, False
 
-    return '{}\033[4;31m{}\033[0m{}'.format(
+    # Right-most whitespace is meaningless
+    line = source_lines[name.line].rstrip()
+
+    # Left-most whitespace is also meaningless, but we have
+    # to be a bit more careful to maintain the correct range
+    # of the highlighted region relative to the line.
+
+    while line and line[0] in [' ', '\t']:
+        line = line[1:]
+        lo, hi = lo - 1, hi - 1
+
+    # While we have more characters than we want AND the size
+    # of the highlighted portion can be reduced to below this
+    # size, try trimming single characters from the end of the
+    # string and leave the highlighted portion somewhere in the
+    # middle.
+
+    while len(line) > MAX_HIGHLIGHT_RANGE and (hi - lo) < MAX_HIGHLIGHT_RANGE:
+        trimmable_lo = lo > 0
+        trimmable_hi = len(line) - hi - 1
+
+        if trimmable_lo > 0 and trimmable_lo >= trimmable_hi:
+            line = line[1:]
+            lo, hi = lo - 1, hi - 1
+            trimmed_lo = True
+
+        if trimmable_hi > 0 and trimmable_hi >= trimmable_lo:
+            line = line[:-1].rstrip()
+            trimmed_hi = True
+
+    return '{}{}\033[4;31m{}\033[0m{}{}'.format(
+        '... ' if trimmed_lo else '',
         line[:lo].lstrip(),
         line[lo:hi],
         line[hi:].rstrip(),
+        ' ...' if trimmed_hi else '',
     )
-
