4aae1e5621d28c30608c88ae0661c50e36ed646f
Eric Fritz <eric@eric-fritz.com>
2019-07-31 16:36:15 -0700

    Add flag to remove content from document vertices.

--- lsif_indexer/script.py
+++ lsif_indexer/script.py
@@ -10,7 +10,7 @@ def main():
     start = time.time()
 
     with open(args.o, 'w+') as f:
-        index(args.workspace, f, args.verbose)
+        index(args.workspace, f, args.verbose, args.exclude_content)
 
     print('\nProcessed in {0:.2f}ms'.format((time.time() - start) * 1000))
 
@@ -20,6 +20,7 @@ def parse_args():
     parser.add_argument('workspace', help='set the path to the code, current directory by default')
     parser.add_argument('-o', help='change the output file, "data.lsif" by default', default='data.lsif')
     parser.add_argument('-v', '--verbose', action='store_true', help='Output verbose logs', default=False)
+    parser.add_argument('--exclude-content', action='store_true', help='Do not emit document content', default=False)
     parser.add_argument('--version', action='version', version='Go LSIF indexer: {}, Protocol version: {}'.format(
         INDEXER_VERSION,
         PROTOCOL_VERSION,
