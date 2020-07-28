e25dba960ebfeddd3f6ab71bfb79d431c9b5b3a4
Alexandre Voiney <1880968+avoiney@users.noreply.github.com>
2020-07-28 06:13:56 +0200

    Add a setup.py file (#9)

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lsif-py",
+    version="0.0.1",
+    author="Eric Fritz",
+    author_email="eric@sourcegraph.com",
+    description="Python LSIF Indexer",
+    entry_points={"console_scripts": ["lsif-py=lsif_indexer.script:main"],},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sourcegraph/lsif-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["jedi==0.16.0"],
+)
